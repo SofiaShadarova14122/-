1f3c1cde9078c767312aa21d8885954f5d9dd1b2
SofiaShadarova14122 <s_shadarova@bk.ru>
2023-01-09 13:23:57 +0800

    class_list

--- 2_1/16_11_22_subtraction.py
+++ 2_1/16_11_22_subtraction.py
@@ -1,26 +1,15 @@
-class Math:
+class Subtraction(list):
+    def __init__(*args):
+        list.__init__(*args)
 
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-        
-    def addition(self):
-        return self.a + self.b
-    
-    def multiplication(self):
-        return self.a * self.b
-    
-    def division(self):
-        return self.a / self.b
-    
-    def subtraction(self):
-        return self.a - self.b
+    def __sub__(self, second_list):
+        result = list(self)
+        for i in range(len(list(second_list))):
+            while second_list[i] in result:
+                result.remove(second_list[i])
+        print(f"List {self} minus list {second_list} ---> list {result}")
 
-a, b = map(float,input("Введите два числа через пробел").split())
 
-arg = Math(a, b)
-
-print('addition = ', arg.addition())
-print('multiplication =', arg.multiplication())
-print('division =', arg.division())
-print('subtraction =', arg.subtraction())
+first_list = Subtraction([3, 5, 1, 4, 3, 7, 9, 9, 0])
+second_list = [5, 3, 7]
+first_list - second_list
